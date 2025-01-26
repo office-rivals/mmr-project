16523d31dd9f9412555c3b77433bc9800d982ffa
Bjarke Hesthaven Søndergaard <bhsgaard@gmail.com>
2025-01-26 23:00:08 +0100

    Enable matchmaking

--- frontend/src/routes/(authed)/matchmaking/+page.svelte
+++ frontend/src/routes/(authed)/matchmaking/+page.svelte
@@ -48,36 +48,41 @@
 
 <PageTitle>Matchmaking</PageTitle>
 
-<p class="my-6">
-  Matchmaking is a feature where you queue up for a game against other people
-  that are also ready for a game. <br />Once {PLAYERS_REQUIRED_FOR_MATCH} players
-  are in the queue, a match will be created and you will be notified.
-  <br />
-  If you do not accept the match within 30 seconds, you will be removed from the
-  queue.
-  <br />
-  A sound will play when a match is found.
-</p>
-<form method="post">
-  {#if isUserInQueue}
-    <input type="hidden" name="intent" value="leave" />
-    <div class="flex gap-2">
-      <LoaderCircle class="animate-spin" />
-      <p>Waiting for a match...</p>
-    </div>
-    <p>Missing {remainingPlayers} more players</p>
-    <Button variant="destructive" type="submit">
-      <Pause /><span class="ml-2">Leave queue</span>
-    </Button>
-  {:else if !data.hasClaimedProfile}
-    <p>You need to link your login to a player to queue up for a match.</p>
-    <Button href="/profile" size="lg" class="w-full py-6 text-xl"
-      >Claim profile</Button
-    >
-  {:else}
-    <input type="hidden" name="intent" value="queue" />
-    <Button size="lg" class="w-full py-6" type="submit">
-      <Play /><span class="ml-2 text-xl">Queue up</span>
-    </Button>
-  {/if}
-</form>
+<div class="mt-6 flex flex-col gap-4">
+  <p>
+    Matchmaking is a feature where you queue up for a game against other people
+    that are also ready for a game.
+  </p>
+  <p>
+    Once <strong>{PLAYERS_REQUIRED_FOR_MATCH} players</strong> are in the queue,
+    a match will be created and you will be notified.
+  </p>
+  <p>
+    If you do not accept the match within <strong>30 seconds</strong>, you will
+    be removed from the queue.
+  </p>
+  <p>A sound will play when a match is found.</p>
+  <form method="post">
+    {#if isUserInQueue}
+      <input type="hidden" name="intent" value="leave" />
+      <div class="flex gap-2">
+        <LoaderCircle class="animate-spin" />
+        <p>Waiting for a match...</p>
+      </div>
+      <p>Missing {remainingPlayers} more players</p>
+      <Button variant="destructive" type="submit">
+        <Pause /><span class="ml-2">Leave queue</span>
+      </Button>
+    {:else if !data.hasClaimedProfile}
+      <p>You need to link your login to a player to queue up for a match.</p>
+      <Button href="/profile" size="lg" class="w-full py-6 text-xl"
+        >Claim profile</Button
+      >
+    {:else}
+      <input type="hidden" name="intent" value="queue" />
+      <Button size="lg" class="w-full py-6" type="submit">
+        <Play /><span class="ml-2 text-xl">Queue up</span>
+      </Button>
+    {/if}
+  </form>
+</div>
