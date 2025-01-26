cd79b633432bec9ed45acfd521f929266a1a440c
Bjarke Hesthaven Søndergaard <bhsgaard@gmail.com>
2025-01-26 22:49:36 +0100

    Add migration

---
+++ backend-dotnet/MMRProject.Api/Data/Migrations/20250126214917_AddAdminFlagToUsers.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MMRProject.Api.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddAdminFlagToUsers : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsAdmin",
+                table: "users",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsAdmin",
+                table: "users");
+        }
+    }
+}
