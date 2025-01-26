e7bea95bb3e69a8b8ec431111e6ebe8eed57930b
Bjarke Hesthaven Søndergaard <bhsgaard@gmail.com>
2025-01-26 23:14:35 +0100

    Provide an error instead of redirecting

--- frontend/src/routes/(authed)/admin/+layout.server.ts
+++ frontend/src/routes/(authed)/admin/+layout.server.ts
@@ -1,10 +1,12 @@
-import { redirect } from '@sveltejs/kit';
+import { error } from '@sveltejs/kit';
 import type { LayoutServerLoad } from './$types';
 
 export const load: LayoutServerLoad = async ({ locals: { apiClient } }) => {
   const permissions = await apiClient.profileApi.profileGetProfilePermissions();
 
   if (permissions.isAdmin == null || !permissions.isAdmin) {
-    redirect(303, '/');
+    error(403, {
+      message: 'You do not have permission to access this page',
+    });
   }
 };
