5c95503cd941351b011c500e80e34f11ed202313
Bjarke Hesthaven Søndergaard <bhsgaard@gmail.com>
2025-10-08 22:37:53 +0200

    Add Clerk and remove Supabase

--- api/MMRProject.Api/Auth/BuilderExtensions.cs
+++ api/MMRProject.Api/Auth/BuilderExtensions.cs
@@ -11,10 +11,6 @@ public static class BuilderExtensions
 
     public static WebApplicationBuilder AddAuth(this WebApplicationBuilder builder)
     {
-        var supabaseSignatureKey = GetSupabaseSecurityKey(builder.Configuration);
-        // var validIssuer =
-        //     builder.Configuration.GetValue<string>("Supabase:Issuer")!; // TODO: Move all this to a configuration class
-
         builder.Services.AddAuthentication(options =>
             {
                 options.DefaultScheme = "MultiAuth";
@@ -35,31 +31,18 @@ public static class BuilderExtensions
             })
             .AddJwtBearer(JwtBearerScheme, o =>
             {
+                var issuer = builder.Configuration.GetValue<string>("Authorization:Issuer");
                 o.TokenValidationParameters = new TokenValidationParameters
                 {
-                    ValidateIssuerSigningKey = true,
-                    ValidateIssuer = false,
-                    ValidateAudience = true,
-                    IssuerSigningKey = supabaseSignatureKey,
-                    ValidAudiences = ["authenticated"],
-                    // ValidIssuer = validIssuer
+                    ValidateIssuer = true,
+                    ValidateAudience = false,
+                    ValidIssuer = issuer,
                 };
+                o.Authority = issuer;
             })
             .AddScheme<PersonalAccessTokenAuthenticationOptions, PersonalAccessTokenAuthenticationHandler>(
                 PatScheme, options => { });
 
         return builder;
-    }
-
-    private static SymmetricSecurityKey GetSupabaseSecurityKey(IConfiguration configuration)
-    {
-        var keyInConfiguration = configuration.GetValue<string>("Supabase:SignatureKey");
-        if (string.IsNullOrEmpty(keyInConfiguration))
-        {
-            // TODO: Better exception
-            throw new Exception("Supabase Signature Key not found in configuration");
-        }
-
-        return new SymmetricSecurityKey(Encoding.UTF8.GetBytes(keyInConfiguration));
     }
 }
